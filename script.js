3f8d27bed3fe05795b9049f56cc22e616bfd1360
Stella <stellagmendonca@gmail.com>
2026-01-27 17:18:59 -0300

    deploy concluido

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://stellag2003.github.io/MiniProjeto---M2/";
+const API_URL = "https://miniprojeto-m2.onrender.com";
 
 // FUNÇÃO: LISTAR (GET) + MANIPULAÇÃO DO DOM
 async function listarTarefas() {
